a3f826d37179ccbfd04bd0b5b845ab650ef6fc27
tylerjusfly <tylerjusfly1@gmail.com>
2022-05-26 13:40:14 +0100

    refactoring code , code a piece , test a piece

--- src/integrated-layer.ts
+++ src/integrated-layer.ts
@@ -22,6 +22,35 @@ export const integrate = async (req: Request, res: Response, next: NextFunction)
         jsonData.cargoAmount,
       ]);
     }
+    // Stopped here
+    if (uid.rowCount === 0 && jsonData.type == "to") {
+      await pool.query("INSERT INTO orders(orderid, toLocation )VALUES($1, $2)", [jsonData.extOrderId, jsonData.toLocation]);
+    }
+    if (uid.rowCount === 0 && jsonData.type == "from") {
+      await pool.query("INSERT INTO orders(orderid, fromLocation )VALUES($1, $2)", [jsonData.extOrderId, jsonData.fromLocation]);
+    }
+
+    // if id exists
+    if (uid.rowCount == 1 && jsonData.type == "to") {
+      await pool.query("UPDATE orders SET toLocation = $1, updated_at = $2 WHERE orderid = $3 AND toLocation IS NULL", [
+        jsonData.toLocation,
+        "Now()",
+        jsonData.extOrderId,
+      ]);
+    }
+    if (uid.rowCount == 1 && jsonData.type == "from") {
+      await pool.query("UPDATE orders SET fromLocation = $1, updated_at = $2 WHERE orderid = $3 AND fromLocation IS NULL", [
+        jsonData.fromLocation,
+        "Now()",
+        jsonData.extOrderId,
+      ]);
+    }
+    if (uid.rowCount == 1 && jsonData.type == "cargo") {
+      await pool.query(
+        "UPDATE orders SET cargoType = $1, cargoAmount = $2, updated_at = $3 WHERE orderid = $4 AND (cargoType, cargoAmount) IS NULL",
+        [jsonData.cargoType, jsonData.cargoAmount, "Now()", jsonData.extOrderId],
+      );
+    }
     // Getting updated/saved data to display
     const savedResult = await pool.query("SELECT * FROM orders WHERE orderid = $1", [jsonData.extOrderId]);
 
